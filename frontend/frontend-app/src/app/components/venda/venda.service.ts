0b66170c0629235c87df0a6481d780b4689703c3
Marcelo  Silva <mrsalustiano@gmail.com>
2020-06-05 14:02:04 -0300

    vendas inicio

--- frontend/frontend-app/src/app/components/venda/venda.service.ts
+++ frontend/frontend-app/src/app/components/venda/venda.service.ts
@@ -1,9 +1,66 @@
+import { Venda } from './venda';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Injectable } from '@angular/core';
+import { Observable, EMPTY } from 'rxjs';
+import { Casa } from '../casa/casa';
+import { map, catchError } from 'rxjs/operators';
+import { Evento } from '../evento/evento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VendaService {
 
-  constructor() { }
+  baseUrl = "http://localhost:8080/vendas"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string , isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 2000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
+    })
+  }
+
+  errorHandle(e: any): Observable<any> {
+    this.showMessage('Ocorreu um Erro' + e, true);
+    return EMPTY;
+
+  }
+
+  createVenda(venda: Venda): Observable<Venda>{
+    return this.http.post<Venda>(this.baseUrl, venda).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandle(e) )
+    )
+  }
+
+
+
+  readVendaById(id: string): Observable<Evento> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Venda>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandle(e) )
+    )
+  }
+
+
+  readCasa(): Observable<Casa[]>{
+    return this.http.get<Casa[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandle(e) )
+    )
+  }
+
+  readEvento(): Observable<Evento[]>{
+    return this.http.get<Evento[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandle(e) )
+    )
+  }
+
 }
--- frontend/frontend-app/src/app/components/venda/venda.ts
+++ frontend/frontend-app/src/app/components/venda/venda.ts
@@ -1,2 +1,11 @@
-export interface Venda {
+import { Cliente } from './../cliente/cliente';
+import { Evento } from './../evento/evento';
+export class Venda {
+   
+	cliente: Cliente;
+	evento: Evento;	
+	quantidade: number;
+    valor: number;
+    
+    
 }
